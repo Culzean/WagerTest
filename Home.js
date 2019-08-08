8c56c03ebbdbdda31541d80bc91649996aad52db
DESKTOP-U22SJMM\culze <culzeanswift@gmail.com>
2019-08-08 13:07:15 +0100

    Adjust style for home page. Using class component and state for getting username.

--- Home.js
+++ Home.js
@@ -1,26 +1,56 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
+  TextInput,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
 } from 'react-native';
 import { Button } from 'react-native-material-ui';
 
 
 class Home extends React.Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userName:'',
+        };
+    }
+
     render() {
         return (
-            <SafeAreaView>
-                <Button raised primary text="Search" onPress={() => {
-                    this.props.navigation.navigate('SearchResultScreen', { userName: 'culzeanmememe' } );
-                }}/>
+            <SafeAreaView style={styleSheet.InputBox}>
+                <View >
+                    <TextInput
+                        style={styleSheet.InputChild}
+                        onChangeText={(text) => {
+                            this.setState({ userName: text});
+                        }}
+                        placeholder={'User name...'}
+                    />
+                    <Button
+                        style={styleSheet.InputChild}
+                        raised primary text="Search" onPress={() => {
+                        this.props.navigation.navigate('SearchResultScreen', { userName: this.state.userName } );
+                    }}/>
+                </View>
             </SafeAreaView>
         );
     }
 }
+
+const styleSheet = StyleSheet.create({
+    InputBox:{
+        flexDirection: 'column',
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    InputChild: {
+        width: 300,
+    },
+});
 
 export default Home;
